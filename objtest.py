265544dff790b9374854a736a797fc3dd8041afd
Kanon Kubose <git@herbaloutfitters.com>
2016-10-06 16:54:38 -0700

    Fooling around with objtest.py

--- objtest.py
+++ objtest.py
@@ -1,12 +1,21 @@
 class Dog:
+    species = 'canine'
+    count = 0
 
     def __init__(self, name, prev, next):
         self.name = name
         self.prev = prev
         self.next = next
+        Dog.count += 1
 
 d = Dog('fido', 'rover', 'chev')
 
 print d.next
 print d.prev
 print d.name
+print "how many dogs: " + str(Dog.count)
+
+anotherdog = Dog('bree', 'fido', 'rover')
+
+print anotherdog.name
+print "how many dogs: " + str(Dog.count)
